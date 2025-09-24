3321cac58f349ff9bcfb9228aad23aa212a4954e
master-skd <LZH1428572021@163.com>
2025-09-24 08:37:15 +0000

    15_5_eval

--- configs/train_naive_voc_15_5/create_dataset.sh
+++ configs/train_naive_voc_15_5/create_dataset.sh
@@ -1,4 +1,4 @@
 python tools/create_incremental_dataset.py \
     --dataset_path data/VOC/VOC.yaml \
-    --output_path data/VOC/VOC_inc_15_5 \
+    --output_path data/VOC_inc_15_5 \
     --n_classes 15 5
